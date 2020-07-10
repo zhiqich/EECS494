4d5f5caa5d14c076d490c74b8652fd0ac6ac828d
Zhiqi Chen <abraxas.zhiqich@gmail.com>
2020-07-10 13:53:51 -0400

    Zelda

---
+++ Zelda/Assets/Scripts/AquamentusHealth.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AquamentusHealth : MonoBehaviour
+{
+    public AudioClip enemy_hit_sound_clip;
+    public AudioClip enemy_die_sound_clip;
+
+    private int life = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (life <= 0)
+        {
+            AudioSource.PlayClipAtPoint(enemy_die_sound_clip, Camera.main.transform.position);
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("sword"))
+        {
+            AudioSource.PlayClipAtPoint(enemy_hit_sound_clip, Camera.main.transform.position);
+            life -= 1;
+        }
+        if (other.CompareTag("arrow"))
+        {
+            AudioSource.PlayClipAtPoint(enemy_hit_sound_clip, Camera.main.transform.position);
+            life -= 1;
+        }
+        if (other.CompareTag("linkboomerange"))
+        {
+            StartCoroutine(Stun());
+            AudioSource.PlayClipAtPoint(enemy_hit_sound_clip, Camera.main.transform.position);
+            life -= 1;
+        }
+        if (other.CompareTag("bomb"))
+        {
+            AudioSource.PlayClipAtPoint(enemy_hit_sound_clip, Camera.main.transform.position);
+            life -= 1;
+        }
+    }
+
+    IEnumerator Stun()
+    {
+        GetComponent<AquamentusMovement>().stun = true;
+
+        float t = Time.time;
+        float progress = (Time.time - t) / 1f;
+        while (progress < 1f)
+        {
+            progress = (Time.time - t) / 1f;
+            GetComponent<SpriteRenderer>().enabled = !GetComponent<SpriteRenderer>().enabled;
+            yield return null;
+        }
+        GetComponent<SpriteRenderer>().enabled = true;
+        GetComponent<AquamentusMovement>().stun = false;
+    }
+}
